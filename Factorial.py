c7d4071a43c931784ab05c4111160a4db75936de
Gaurav Padawe <grvpdw92@gmail.com>
2018-06-04 22:48:58 +0530

    Initial commit

---
+++ Factorial.py
@@ -0,0 +1,8 @@
+﻿def factorial(n):             # Defining a procedure factorial
+    i = 1                     # Creating reference variable "i" having value 1
+    a = range(n, 1, -1)       # Setting range by passing value of n which will go till 1 by -1 unit
+    for x in a:               # Getting individual values of a
+        i = i * x             # i will get updated every time in for loop with new value of a
+    return i
+    
+print factorial(10)
